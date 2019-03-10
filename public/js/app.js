d29f648a16be92410605b1c4f5110804a71ef5e7
Smill Vásquez <smillalexander@gmail.com>
2019-03-10 12:10:42 -0400

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherFrom = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+
+weatherFrom.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value;
+    messageOne.textContent = 'Loading......';
+    messageTwo.textContent = '';
+    fetch(`/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = '';
+                messageTwo.textContent = data.error;
+
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        })
+    }).catch((error) => {
+        console.log(error);
+    });
+    console.log(location);
+
+}, false);
